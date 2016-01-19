6030c7f6c5daa28518bebdf08f078fc84e8e2645
Ryan O'Keefe <rpokeefe@umich.edu>
2016-01-19 15:07:45 -0500

    adding lab 2 files important for postlab

---
+++ lab2/Debug/sources.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+O_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+S_SRCS := 
+OBJ_SRCS := 
+MEMORYMAP := 
+OBJS := 
+C_DEPS := 
+SRECFILES := 
+IHEXFILES := 
+LISTINGS := 
+EXECUTABLE := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+
+################################################################################
+# Microsemi SoftConsole IDE Variables
+################################################################################
+
+BUILDCMD = arm-none-eabi-gcc -mthumb -mcpu=cortex-m3 -specs=bare.specs -nostartfiles -nodefaultlibs -nostdlib -static -T../link.ld -Wl,-Map=$(EXECUTABLE).map 
+SHELL := cmd.exe
+EXECUTABLE := lab2
+
+# Target-specific items to be cleaned up in the top directory.
+clean::
+	-$(RM) $(wildcard ./*.map) 
