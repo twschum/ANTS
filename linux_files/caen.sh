84089f9b51974d5d2ea4cf512f9bde1eaa8fdfaf
Tim <tim@deepfield.net>
2016-01-21 12:17:02 -0500

    updated caen script

--- linux_files/caen.sh
+++ linux_files/caen.sh
@@ -1,45 +1,53 @@
 #!/bin/bash
 
-# Check runtime argument
-if [ ! $# == 1 ]; then
-	echo "Usage: $caen <uniquename>"
+helpstr="Usage: $caen <uniquename> <ssh|mount|unmount> [host]"
+
+# Check runtime arguments
+if [[  $# != 2 && $# != 3 ]]; then
+	echo $helpstr
 	exit
 fi
 
-# CAEN login access point:
-# Dependant on argument1 uniquename
-uniquename=$1
-caenhost="login-course.engin.umich.edu"
+
+uniquename="$1"
+action="$2"
+host="login-course"
+if [ $# == 3 ]; then
+	host="$3"
+fi
+
+caendomain="engin.umich.edu"
+caenhost="$host.$caendomain"
 
 # This will allow quck access to CAEN ssh or sshfs
 
-echo -e "\n CAEN Access script by Tim Schumacher "
-echo -e " [1] Access CAEN via ssh "
-echo -e " [2] Mount CAEN directory to ~/"$uniquename
-echo -e " [q] exit "
+#echo -e "\n CAEN Access script by Tim Schumacher "
+#echo -e " [1] Access CAEN via ssh "
+#echo -e " [2] Mount CAEN directory to ~/"$uniquename
+#echo -e " [q] exit "
 
-read -p "% " in
+#read -p "% " in
 
 # Open ssh to CAEN
-if [ $in == 1 ]; then
+if [ $action == "ssh" ]; then
 	# ssh command to access CAEN
-	echo -e "Acessing CAEN login for "$uniquename'@'$caenhost"\n"
+	echo -e "Acessing CAEN login for "$uniquename'@'$caenhost
 	ssh -L 5951:localhost:5951 $uniquename'@'$caenhost
 
 # Mount home CAEN dir via sshfs
-elif [ $in == 2 ]; then
+elif [ $action == "mount" ]; then
 
-	mount_point=~/$uniquename
+	mount_point=$HOME/$uniquename
 	# Check to see if the directory exists
 	if [ ! -d $mount_point ]; then
 		echo "Preparing virtual dir "$mount_point
 		mkdir $mount_point
-	#else
+	else
 		# check if this directory is empty
-		#if["$(ls -A $mount_point)"]; then 
-		#	echo "Directory "$mount_point" not empty, aborting mount..."
-		#	exit
-		#fi
+		if [ ! "$(ls -A $mount_point)" == "" ]; then
+			echo "Directory "$mount_point" not empty, aborting mount..."
+			exit
+		fi
 	fi
 
 	# Open the sshfs connection to that directory
@@ -47,18 +55,23 @@ elif [ $in == 2 ]; then
 	sshfs $uniquename'@'$caenhost: $mount_point
 
 	# Now wait for user to unmount directory
-	echo -e "Drive mounted.  Press [u] then [ENTER] to unmount."
-	while true; do
-	read -p "% " in
-	if [ "$in" == 'u' ];then
+	echo -e "Drive mounted. To unmount:\n caen $uniquename unmount"
+
+# unmount the mounted caen drive
+elif [ $action == "unmount" ]; then
+
+	mount_point=$HOME/$uniquename
+	if [ -d $mount_point ]; then
+		echo Unmounting $mount_point
 		fusermount -u $mount_point
 		rmdir $mount_point
-		exit
+	else
+		echo $mount_point does not exist
 	fi
-	done
 
-# quit
-elif [ "$in" == 'q' ];then
+# unknown / incorrect arguments
+else
+	echo $helpstr
 	exit
 fi
 
