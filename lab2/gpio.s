b82e4271c59443147ca84bce468ff98dc3c98c97
Tim <tim@deepfield.net>
2016-01-21 13:00:18 -0500

    wrote the assembly to blink all the leds

--- lab2/gpio.s
+++ lab2/gpio.s
@@ -14,17 +14,17 @@ initGPIO:
 	@ Load GPIO_OUT_BASE address
 	movw	r1, #:lower16:GPIO_OUT_BASE
 	movt	r1, #:upper16:GPIO_OUT_BASE
-	
+
 	@ Calculate the GPIOx register address
-				@ left shift by 2 => i*4
-				@ GPIO_OUT_BASE + i*4
+	@ left shift by 2 => i*4
+	@ GPIO_OUT_BASE + i*4
 	lsl r0, #2
 	add r0, r0, r1
-				
+
 	@ Write 1 to config register to set GPIO as output
 	mov r2, #1
 	str r2, [r0]
-	
+
 	bx	lr @ Return
 
 @ Set the value of all 32 GPIO output bits based on the input bits
@@ -36,9 +36,9 @@ setGPIO:
 	@ Load GPIO_OUT register address
 	movw	r1, #:lower16:GPIO_OUT
 	movt	r1, #:upper16:GPIO_OUT
-	
+
 	@ Write 32bit value to GPIO output register
 	str r0, [r1]
-	
+
 	bx	lr @ Return
-	
+
