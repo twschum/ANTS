978e1cf8e19ce8a7ad2fe5590b7d34eb2ceb8722
Tim <tim@deepfield.net>
2016-02-16 14:02:32 -0500

    finished the timer_t connector

--- lab_files/timer/timer_t.c
+++ lab_files/timer/timer_t.c
@@ -5,52 +5,77 @@
 #define PERIODIC 0
 
 // pointer to start of linked list
-timer_t *root = NULL;
+struct Timer* root = NULL;
+
+void Timer1_IRQHandler(void) {
+
+    handler_t handler = root->handler;
+
+    update_timers();
+
+    MSS_TIM1_clear_irq();
+
+    // if not empty, start the timer again
+    if (root) {
+        MSS_TIM1_load_immeadiate(root->time_left);
+        MSS_TIM1_start();
+    }
+
+    // call the timer's handler last
+    handler();
+}
 
 void start_hardware_timer(uint32_t period) {
 
+    /*
     MSS_TIM1_init(MSS_TIMER_ONE_SHOT_MODE);
     MSS_TIM1_load_immediate(period);
     MSS_TIM1_start();
     MSS_TIM1_enable_irq();
+    */
+
+    if (period) {
+        period = 0;
+    }
+
 }
 
 // put new timer in list maintaining order least time remaining to most
-void insert_timer(Timer* newtimer) {
+void insert_timer(struct Timer* newtimer) {
 
+    // insert empty case
     if (root == NULL) {
         root = newtimer;
         return;
     }
 
     // front insert case
-    if (newtimer->index < root->index) {
-        newtimer->next = head;
+    if (newtimer->time_left < root->time_left) {
+        newtimer->next = root;
         return;
     }
 
+    struct Timer* pos = root;
+
     // pos will be the "insert after" location list_t* pos = head;
-
-    while (pos->next != NULL && pos->next->index < newtimer->index) {
-
+    while (pos->next != NULL && pos->next->time_left < newtimer->time_left) {
         pos = pos->next;
     }
 
     // insert after pos
     newtimer->next = pos->next;
     pos->next = newtimer;
-
-    return head;
 }
 
 void add_timer(handler_t handler, uint32_t period, uint32_t mode) {
 
-    Timer* newtimer = malloc(sizeof(Timer));
+    struct Timer* newtimer = malloc(sizeof(struct Timer));
 
     newtimer->handler = handler;
     newtimer->time_left = period;
     newtimer->period = period;
     newtimer->mode = mode;
+    newtimer->next = NULL;
 
     insert_timer(newtimer);
 }
@@ -65,8 +90,31 @@ void add_timer_single(handler_t handler, uint32_t period) {
     add_timer(handler, period, ONE_SHOT);
 }
 
-
-
 // update down count with elapsed time, call fnc if timer zero,
 // update continuous timers with new down count
-void update_timers(void) {};
+void update_timers(void) {
+
+    // first, update all the time_left fields
+    struct Timer* node = root;
+    uint32_t elapsed = root->time_left;
+
+    while (node) {
+        node->time_left -= elapsed;
+        node = node->next;
+    }
+
+    // second, deal with the root timer
+    struct Timer* head = root;
+    root = head->next;
+
+    if (head->mode == ONE_SHOT) {
+        free(head);
+    }
+
+    else {
+        head->time_left = head->period;
+        head->next = NULL;
+        insert_timer(head);
+    }
+}
+
--- lab_files/timer/timer_t.h
+++ lab_files/timer/timer_t.h
@@ -1,23 +1,27 @@
 // timer_t.h virtual timer firmware
 
+#ifndef TIMER_T_H
+#define TIMER_T_H
+
 #include <inttypes.h>
 #include <stdlib.h>
 
 #include "mss_timer.h"
 
+void Timer1_IRQHandler(void);
+
+
 // declare function pointer type
 typedef void (*handler_t)(void);
 
 // virtual down Timer
-typedef struct {
+struct Timer {
   handler_t handler;    // function pointer (called after timer period)
   uint32_t time_left;   // time remaining for this counter
   uint32_t period;      // period
   uint32_t mode; // continuous or one shot timer
-  struct Timer *next;   // points to next timer
-} Timer;
-
-
+  struct Timer* next;   // points to next timer
+};
 
 // used to initialize hardware
 // using MSS_TIM1
@@ -32,3 +36,5 @@ void add_timer_single(handler_t handler, uint32_t period);
 // update down count with elapsed time, call fnc if timer zero,
 // update continuous timers with new down count
 void update_timers(void);
+
+#endif // TIMER_T_H
