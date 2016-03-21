e10cde3b865310b2746b8eb53e4509bd00600026
Tim <tim@deepfield.net>
2016-03-21 18:16:15 -0400

    Wrote the rest of the write command module

--- n64_write_command.v
+++ n64_write_command.v
@@ -1,13 +1,58 @@
 
 module n64_write_command(
     input reg [7:0] command_byte;
-    input reg enable;
+    input reg en;
     input clk;
     output reg data_out;
-    output reg [2:0] index;
+    output reg writing_data;
 );
 
+reg enabled;
+reg [8:0] count;
 
+parameter START = 100;
+parameter DATA = 300;
+parameter STOP = 400;
 
+assign writing_data = enabled;
+
+always @ (posedge clk) begin
+
+    // latch the command byte when enable goes high
+    if (en & ~enabled) begin
+        enabled <= 1;
+        index <= 0
+        count <= 0;
+    end
+
+    // counter & reset logic
+    if (enabled & count < STOP) begin
+        count <= count + 1;
+
+    else if (enabled & count == STOP & index == 8)
+        enabled <= 0;
+
+    else if (enabled & count == STOP)
+        count <= 0;
+        index <= index + 1;
+    end
+
+    // outputs based on current state (count)
+    if (enabled) begin
+
+        if (count < START) begin
+            data_out <= 0;
+        else if (count < DATA)
+            data_out <= command_byte[index];
+        else if (count < STOP)
+            data_out <= 1;
+        end
+
+    else
+        index <= 0;
+        data_out <= 1;
+    end
+
+end
 
 endmodule
