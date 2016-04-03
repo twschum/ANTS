f4bc9551beeccd948bd3f836f1e4b57136121786
Tim <tim@deepfield.net>
2016-04-03 16:29:40 -0400

    Now using intuitive D-pad control

--- ants_master/SoftConsole/ants_master_MSS_MSS_CM3_0/ants_master_MSS_MSS_CM3_0_app/drivers/servo_control.c
+++ ants_master/SoftConsole/ants_master_MSS_MSS_CM3_0/ants_master_MSS_MSS_CM3_0_app/drivers/servo_control.c
@@ -5,10 +5,58 @@ void set_x_servo(uint32_t cycles) {
 
     volatile uint32_t* address = (volatile uint32_t*)X_SERVO_ADDR;
     *address = cycles;
+
+    x_servo = cycles;
+    printf("X servo set to: %d\r\n", x_servo);
 }
 
 void set_y_servo(uint32_t cycles) {
 
     volatile uint32_t* address = (volatile uint32_t*)Y_SERVO_ADDR;
     *address = cycles;
+
+    y_servo = cycles;
+    printf("Y servo set to: %d\r\n", y_servo);
+}
+
+uint32_t increment_forward_speed(uint32_t pos) {
+    if (pos == SERVO_FULL_REVERSE) {
+        return SERVO_HALF_REVERSE;
+    }
+    else if (pos == SERVO_HALF_REVERSE) {
+        return SERVO_NEUTRAL;
+    }
+    else if (pos == SERVO_NEUTRAL) {
+        return SERVO_HALF_FORWARD;
+    }
+    else if (pos == SERVO_HALF_FORWARD) {
+        return SERVO_FULL_FORWARD;
+    }
+    else if (pos == SERVO_FULL_FORWARD) {
+        return SERVO_FULL_FORWARD;
+    }
+    else {
+        return pos;
+    }
+}
+
+uint32_t increment_backward_speed(uint32_t pos) {
+    if (pos == SERVO_FULL_REVERSE) {
+        return SERVO_FULL_REVERSE;
+    }
+    else if (pos == SERVO_HALF_REVERSE) {
+        return SERVO_FULL_REVERSE;
+    }
+    else if (pos == SERVO_NEUTRAL) {
+        return SERVO_HALF_REVERSE;
+    }
+    else if (pos == SERVO_HALF_FORWARD) {
+        return SERVO_NEUTRAL;
+    }
+    else if (pos == SERVO_FULL_FORWARD) {
+        return SERVO_HALF_FORWARD;
+    }
+    else {
+        return pos;
+    }
 }
--- ants_master/SoftConsole/ants_master_MSS_MSS_CM3_0/ants_master_MSS_MSS_CM3_0_app/drivers/servo_control.h
+++ ants_master/SoftConsole/ants_master_MSS_MSS_CM3_0/ants_master_MSS_MSS_CM3_0_app/drivers/servo_control.h
@@ -15,9 +15,16 @@
 #define SERVO_HALF_FORWARD 175000
 #define SERVO_FULL_FORWARD 200000
 
+static uint32_t x_servo=SERVO_NEUTRAL, y_servo=SERVO_NEUTRAL;
+
 // set the x position using the defined values
 void set_x_servo(uint32_t pos);
 
 // set the y position using the defined values
 void set_y_servo(uint32_t pos);
+
+// move thru the servo half and full speeds
+// incrementing based on last pos
+uint32_t increment_forward_speed(uint32_t pos);
+uint32_t increment_backward_speed(uint32_t pos);
 
