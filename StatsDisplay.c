6a71a2ffd588346ac9b1ed31dc24707e180c9dd6
jcgknudson <jknudson@umich.edu>
2016-04-05 14:09:32 -0400

    need 2 test
    just saving changes

--- StatsDisplay.c
+++ StatsDisplay.c
@@ -12,17 +12,35 @@ void initDisplay(){
 	LCD_drawBox(N64_DBG_BOX_X1, N64_DBG_BOX_Y1, N64_DBG_BOX_X2, N64_DBG_BOX_Y2, SET);
 	//Draw pos data
 	LCD_setPos(TARGET_HORZ_X, TARGET_HORZ_Y);
-	LCD_printStr("X:");
+	LCD_printStr(TARGET_HORZ_STR);
 	LCD_setPos(TARGET_VERT_X, TARGET_VERT_Y);
-	LCD_printStr("Y:");
+	LCD_printStr(TARGET_VERT_STR);
 	//Draw shots left
 	LCD_setPos(SHOTS_LEFT_POS_X, SHOTS_LEFT_POS_Y);
-	LCD_printStr("Shots:");
+	LCD_printStr(SHOTS_STR);
 	//Draw mode indicator
 	LCD_setPos(MODE_POS_X, MODE_POS_Y);
-	LCD_printStr("Mode:");
+	LCD_printStr(MODE_STR);
 }
 
-/*void testBarebones(10, 10, 1){
+void updateDisplay(lcd_screen_state_t* lcd_state, n64_state_t* ctrlr_state){
+	//Update the N64 Debug Box
 
-}*/
+	//Update the position data
+	LCD_setPos(SHOTS_LEFT_POS_X + (SHOTS_STR_SZ-1) * CHAR_WIDTH, SHOTS_LEFT_POS_Y)
+	
+
+	//Update the shots left
+	//Calculate offset (-1 b/c of null char)
+	LCD_setPos(SHOTS_LEFT_POS_X + (SHOTS_STR_SZ-1) * CHAR_WIDTH, SHOTS_LEFT_POS_Y)
+	LCD_printNum(lcd_state->shots_left);
+
+	//Update the mode
+	LCD_setPos(MODE_POS_X, MODE_POS_Y);
+	if(lcd_state->target_mode){
+		LCD_printStr(AUTO_STR);
+	} else {
+		LCD_printStr(MANUAL_STR);
+	}
+}
+
--- StatsDisplay.h
+++ StatsDisplay.h
@@ -3,48 +3,76 @@
 
 #include <inttypes.h>
 #include "LCD.h"
-//#include "drivers/n64_driver.h"
+#include "n64_driver.h"
 
-//POSITIONING MACROS//////////////////////////
-#define	TARGET_BOX_X1 		69				//
-#define TARGET_BOX_Y1		37				//
-#define TARGET_BOX_X2		159				//
-#define	TARGET_BOX_Y2		127				//
-											//
-#define TARGET_HORZ_X		78				//
-#define TARGET_HORZ_Y		31				//
-											//
-#define TARGET_VERT_X		118				//
-#define TARGET_VERT_Y		31				//
-											//
-//These are here to clear the positioning 	//
-//data from the screen on refresh			//
-#define TARGET_POS_UL_X		78				//
-#define TARGET_POS_UL_Y		31				//
-#define TARGET_POS_LR_X		158				//
-#define TARGET_POS_LR_Y		29				//				
-											//
-#define N64_DBG_BOX_X1		0				//
-#define N64_DBG_BOX_Y1		0				//
-#define N64_DBG_BOX_X2		100				//
-#define N64_DBG_BOX_Y2		12				//
-											//
-#define SHOTS_LEFT_POS_X	10				//
-#define SHOTS_LEFT_POS_Y	110				//
-											//
-#define MODE_POS_X			10				//
-#define MODE_POS_Y			80				//
-											//
-#define PRESSURE_POS_X		0				//
-#define PRESSURE_POS_Y		0				//
-											//
-#define SET 				1 				//
-#define UNSET				0				//
-//////////////////////////////////////////////
+//POSITIONING MACROS//////////////////////////////
+#define	TARGET_BOX_X1 		69					//
+#define TARGET_BOX_Y1		37					//
+#define TARGET_BOX_X2		159					//
+#define	TARGET_BOX_Y2		127					//
+												//
+#define TARGET_HORZ_POS_X	78					//
+#define TARGET_HORZ_POS_Y	31					//
+												//
+#define TARGET_VERT_POS_X	118					//
+#define TARGET_VERT_POS_Y	31					//
+												//
+//These are here to clear the positioning 		//
+//data from the screen on refresh				//
+#define TARGET_UL_POS_X		78					//
+#define TARGET_UL_POS_Y		31					//
+#define TARGET_LR_POS_X		158					//
+#define TARGET_LR_POS_Y		29					//				
+												//
+#define N64_DBG_BOX_X1		0					//
+#define N64_DBG_BOX_Y1		0					//
+#define N64_DBG_BOX_X2		100					//
+#define N64_DBG_BOX_Y2		12					//
+												//
+#define SHOTS_LEFT_POS_X	10					//
+#define SHOTS_LEFT_POS_Y	110					//
+												//
+#define MODE_POS_X			10					//
+#define MODE_POS_Y			80					//
+												//
+#define PRESSURE_POS_X		0					//
+#define PRESSURE_POS_Y		0					//
+												//
+#define SET 				1 					//
+#define UNSET				0					//
+												//
+#define AUTO_MODE			1 					//
+#define MANUAL_MODE 		0 					//
+												//
+#define MODE_STR 			"Mode:"				//
+#define MODE_STR_SZ			6 					//
+												//
+#define AUTO_STR			"AUTO-TARGETING"	//
+#define AUTO_STR_SZ 		15					//
+												//
+#define MANUAL_STR			"MANUAL TARGETING"	//
+#define MANUAL_STR_SZ		17					//
+												//
+#define TARGET_HORZ_STR 	"X:"				//
+#define TARGET_HORZ_STR_SZ	3 					//
+												//
+#define TARGET_VERT_STR		"Y:"				//
+#define TARGET_VERT_STR_SZ	3 					//
+												//
+#define SHOTS_STR			"Shots:"			//
+#define SHOTS_STR_SZ		7 					//
+												//
+#define CHAR_WIDTH			6 					//
+#define CHAR_HEIGHT			8 					//	
+												//
+												//
+//////////////////////////////////////////////////
 
 typedef struct{
-	uint8_t start;
-	uint8_t end;
+	uint8_t start_x;
+	uint8_t start_y;
+	uint8_t end_x;
+	uint8_t end_y;
 } line_t;
 
 typedef struct {
@@ -65,26 +93,18 @@ typedef struct{
 	uint8_t y;
 } pixel_t;
 
+typedef pixel_t point_t;
+
 typedef struct {
-	/*line_t* 	lines;
-	circle_t* 	circles;
-	box_t* 		boxes;
-	pixel_t* 	set_pixels;
-
-	uint8_t 	num_lines;
-	uint8_t 	num_circles;
-	uint8_t 	num_boxes; 
-	uint8_t 	num_set_pixels; */
-
 	//Pointer to the shape representing the position of the laser pointer
 	circle_t*	target_pos;
 	circle_t*	turret_pos;
-
+	uint8_t 	shots_left;
+	uint8_t		target_mode; 		
 } lcd_screen_state_t;
 
 
-
-//static void updateDisplay();
+void updateDisplay(lcd_screen_state_t* lcd_state, n64_state_t* ctrlr_state);
 
 void initDisplay();
 
